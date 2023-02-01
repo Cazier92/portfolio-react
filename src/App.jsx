8a2ec1e46ed6fece60fc79c3a33ce3a770c35690
Cazier92 <bryce.cazier@gmail.com>
2023-01-31 21:19:46 -0800

    scaffold out app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { useState } from 'react';
+
+// Styling
+import './App.css';
+
+// Components
+
+
+function App() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
