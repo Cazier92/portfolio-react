e21aa3030bce3bad8ba64b1348e09476e9eba546
Cazier92 <bryce.cazier@gmail.com>
2023-02-01 21:46:51 -0800

    add starter bio

--- src/pages/Bio.jsx
+++ src/pages/Bio.jsx
@@ -9,9 +9,15 @@ const Bio = (props) => {
         <h1>About</h1>
         <div className='bio-img-text'>
           <img src={BioImage} alt="" className='bio-img'/>
-          <p className='bio-text'>
-            
-          </p>
+            <p className='bio-text'>
+              Hello! My name is Bryce Cazier, welcome to my portfolio. I'm a Fullstack Developer, experienced in HTML, CSS, JavaScript, Node.js, Express, Mongoose, MongoDB, and React. In my Projects section, you'll find links to various apps I've developed. 
+              <br /> 
+              <br />
+              My dog, Kaia is the most important thing in the world to me. She goes everywhere with me! She's a Siberian Husky puppy and a real handful, but I love her despite all the trouble she gets into, which has granted her the nickname "Miss Mayhem".
+              <br />
+              <br />
+              
+            </p>
         </div>
       </div>
     </>
